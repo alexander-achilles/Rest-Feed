6ffcfeedd6e99ae123e3f05b5931eedb6597df50
alexander-achilles <mrinmoym247@gmail.com>
2021-01-18 23:58:32 +0530

    Set up post model and added server side validation

--- Rest-API/app.js
+++ Rest-API/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose= require('mongoose');
+
+require('dotenv').config();
 
 const feedRoutes = require('./routes/feed');
+const { Mongoose } = require('mongoose');
 
 const app = express();
 
@@ -17,6 +21,10 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080,()=>{
+mongoose.connect('mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.pwkc0.mongodb.net/Feed?retryWrites=true&w=majority',{useUnifiedTopology: true,  useNewUrlParser: true  }).then(
+    app.listen(8080,()=>{
     console.log("server is up on 8080");
-});
+})
+)
+.catch(err=>{console.log(err);})
+
