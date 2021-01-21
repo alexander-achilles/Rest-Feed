a152cb61c748b1395a712d3eeb494898cccf6b1a
alexander-achilles <mrinmoym247@gmail.com>
2021-01-21 15:53:10 +0530

    Added authorization middleware to all routes

--- Rest-API/routes/feed.js
+++ Rest-API/routes/feed.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const {body}= require('express-validator/check');
 
+const isAuth= require('../middleware/is-auth');
+
+
 const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/posts',[
+router.get('/posts',isAuth, [
     body('title').trim().isLength({min: 5}),
     body('content').trim().isLength({min: 5})
 ], feedController.getPosts);
 
 // POST /feed/post
-router.post('/post', feedController.createPost);
+router.post('/post',isAuth, feedController.createPost);
 
-router.get('/post/:postId',feedController.getPost);
+router.get('/post/:postId',isAuth,feedController.getPost);
 
-router.put('/post/:postId',[
+router.put('/post/:postId',isAuth,[
     body('title').trim().isLength({min: 5}),
     body('content').trim().isLength({min: 5})
 ], feedController.updatePost);
 
-router.delete('/post/:postId',feedController.deletePost);
+router.delete('/post/:postId',isAuth,feedController.deletePost);
 
 module.exports = router;
